99e08ce788ed71b3563657047d5036ee45c37080
Jerika Kanz <jerikacapila@gmail.com>
2021-12-08 19:56:11 +0100

    Outsourcing Javascript Code Into External Files

---
+++ app.js
@@ -0,0 +1,8 @@
+let greetingText = "Hi, I'm Jerk";
+let num = 32;
+alert(greetingText);
+alert(greetingText);
+greetingText = "Hi, I am really Jerk!";
+alert(greetingText);
+alert(num);
+alert(num);
