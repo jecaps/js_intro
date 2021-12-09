147747363d150755587c99ed9c839832c69c06f7
Jerika Kanz <jerikacapila@gmail.com>
2021-12-09 09:56:02 +0100

    Introducing Functions

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ let job = {
   salary: 50000,
 };
 
-let adultYears = age - 18;
+let adultYears;
+
+function calculateAdultYears() {
+  adultYears = age - 18;
+}
+
+calculateAdultYears();
 alert(adultYears);
 
 age = 45;
-adultYears = age - 18;
+calculateAdultYears();
 alert(adultYears);
