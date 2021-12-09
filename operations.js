c2e5dbca2bbbbcdef1884a2f6c16a5cfddcba497
Jerika Kanz <jerikacapila@gmail.com>
2021-12-09 14:48:04 +0100

    Math Operations & Math Rules

--- operations.js
+++ operations.js
@@ -7,3 +7,19 @@ console.log(10 / 4);
 // numbers with decimals are called FLOATS
 
 console.log(10 % 4); // returns the remainder
+
+console.log((10 + 3 - 5) * 10); // PEMDAS rule is applied
+
+let result = (10 + 3 - 5) * 10;
+console.log(result);
+
+result = 10 * 4;
+console.log(result);
+
+result++; // result = result + 1
+result--; // result = result -1
+result += 5; // result = result + 5
+result -= 5; // result = result - 5
+result *= 5; // result = result * 5
+result /= 5; // result = result / 5
+console.log(result);
